982cff071a1a6e4f392bdf6e517f9697a551e89a
anhtp <14520040@gm.uit.edu.vn>
2018-04-26 11:57:46 +0700

    Fixed issue #3: vcvarsall.bat changes current working directory after being called.

--- build.cmd
+++ build.cmd
@@ -1,7 +1,10 @@
 @echo off
+echo %cd%
+set VSCMD_START_DIR=%cd%
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
 set compilerflags=/Od /Zi /EHsc
 set linkerflags=/OUT:"main.exe"
+echo %cd%
 cl.exe %compilerflags% ^
 ./src/main.cpp ^
 ./src/00HelloWorld/HelloWorld.cpp ^
